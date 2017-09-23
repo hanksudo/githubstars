3c0d884d67c3ff88b326b131dae1a3d03b613c01
hanksudo <drapho@gmail.com>
2017-09-24 04:42:47 +0900

    add travis

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
